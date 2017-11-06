466ff15c51c06a693ebb21033cd0ab50d047afd7
kkloke <kokkeong.loke@modefair.com>
2017-11-06 14:59:56 +0800

    Revise class name.

--- code/database/migrations/2017_11_05_121437_modify_student_grades_table.php
+++ code/database/migrations/2017_11_05_121437_modify_student_grades_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ModifyStudentGradesTable extends Migration
+class ModifyStudentGradesTable2 extends Migration
 {
     /**
      * Run the migrations.
